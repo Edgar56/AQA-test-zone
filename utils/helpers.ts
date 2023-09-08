e10fbafe94f7b16ec60f3a0651a428bcb034c54c
Edgar-Chalilov <edgar.chalilov@intapp.com>
2023-09-08 09:31:42 +0300

    Update code style by prettier

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,13 +1,20 @@
-import {Page} from "@playwright/test";
+import { Page } from '@playwright/test'
 
-export  async function navigateTo(page: Page, navBarName: string, navBarCategory?: string, navBarOption?: string) {
-    await page.getByRole('button', { name: `${navBarName}` }).click();
-    await page
-        .locator(`#menu-header${navBarName.toLowerCase()}${navBarCategory.toLowerCase()}`)
-        .filter({hasText: `${navBarOption}`})
-        .getByRole('link', {name: `${navBarOption}`})
-        .click()
+export async function navigateTo(
+  page: Page,
+  navBarName: string,
+  navBarCategory?: string,
+  navBarOption?: string
+) {
+  await page.getByRole('button', { name: `${navBarName}` }).click()
+  await page
+    .locator(
+      `#menu-header${navBarName.toLowerCase()}${navBarCategory.toLowerCase()}`
+    )
+    .filter({ hasText: `${navBarOption}` })
+    .getByRole('link', { name: `${navBarOption}` })
+    .click()
 }
 export async function openCard(page: Page, cardTitle: string) {
-    await page.getByRole('heading', {name: `${cardTitle}`}).click()
+  await page.getByRole('heading', { name: `${cardTitle}` }).click()
 }
